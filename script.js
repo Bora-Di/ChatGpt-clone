2c6820c5ca0be9541a7c7cb687d8be5e4dc7229e
Bora-Di <mbou7841@gmail.com>
2024-07-28 01:55:11 +0100

    lightmode+ start js

---
+++ script.js
@@ -0,0 +1,27 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+
+let userText = null;
+
+const createElement = (html, className) =>{
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = html;
+    return chatDiv;
+}
+
+const handleOutgoingChat = () =>{
+    userText = chatInput.value.trim();
+    const html = `
+                <div class="chat-content">
+                    <div class="chat-details">
+                        <img src="img/user.png" alt="">
+                        <p>${userText}</p>
+                    </div>
+                 </div>`;
+    const outgoingChatDiv = createElement(html, "outgoing");
+    chatContainer.appendChild(outgoingChatDiv);
+}
+
+sendButton.addEventListener("click", handleOutgoingChat);
